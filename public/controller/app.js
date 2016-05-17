360b9744a0cb3b9444bd5239d8f52dd5bc7e65c1
linux <endlessveins@gmail.com>
2016-05-17 20:22:51 +0545

    first commit

---
+++ public/controller/app.js
@@ -0,0 +1,30 @@
+/**
+ * Created by linux on 5/17/16.
+ */
+
+var app= angular.module('knowplaces', ['ui.router']);
+
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            //abstract: true,
+            url: '/home',
+            templateUrl: 'templates/homepage.html'
+        })
+
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'templates/app.html',
+
+        })
+
+
+});
